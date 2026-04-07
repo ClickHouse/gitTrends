50e020a6a7c2c6ac15c62db5c9bfadc7e74a2b45
Lionel Palacin <lionel.palacin@clickhouse.com>
2026-04-07 16:20:43 +0100

    align cookie with clickhouse.com

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Providers from './providers'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter', display: 'block' })
 
@@ -14,6 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={inter.variable}>
+      <GoogleTagManager gtmId="GTM-T55CC768" />
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
